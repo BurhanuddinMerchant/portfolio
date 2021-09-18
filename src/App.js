1ad44977c19400cb09fae7ac868d4839b035ef78
Burhanuddin Merchant <bmerchant945@gmail.com>
2021-09-19 01:59:49 +0530

    imported all the sections

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { NavBar } from "./components/NavBar";
+import { Contact } from "./pages/Contact/Contact";
+import { Education } from "./pages/Education/Education";
+import { Experiences } from "./pages/Experiences/Experiences";
+import { Footer } from "./pages/Footer/Footer";
+import { Home } from "./pages/Home/Home.js";
+import { Organizations } from "./pages/Organizations/Organizations";
+import { Projects } from "./pages/Projects/Projects";
+import { Skills } from "./pages/Skills/Skills";
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Home id="home" />
+      <Skills id="skills" />
+      <Experiences id="experiences" />
+      <Projects id="projects" />
+      <Organizations id="organizations" />
+      <Education id="education" />
+      <Contact id="contact" />
+      <Footer />
+    </>
   );
 }
 
